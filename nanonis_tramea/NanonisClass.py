26657de89815952a9dce230af4f3df7f5f2dfa9d
Aitzbr <aitziber.herrero@specs-zurich.com>
2025-01-14 13:57:59 +0100

    Add files via upload
    Fixed the indentation of two Script functions (Script.Open and Script.LUTOpen) which triggered an error when trying to use the class.
    Changed the behavior of the returnDebugInfo function so that nothing is printed out when there is no error.

--- nanonis_tramea/NanonisClass.py
+++ nanonis_tramea/NanonisClass.py
@@ -184,7 +184,8 @@ class Nanonis:
             print(Recv_Header)
             print(Recv_Body)
         if Recv_Command == Command:
-            print('Correct Command.')
+            if (self.displayInfo == 1):
+                print('Correct Command.')
             return Recv_Body
         else:
             print('Wrong Command')
@@ -324,7 +325,8 @@ class Nanonis:
             print('The following error appeared:', "\n", ErrorString)
             return [ErrorString, Response, Variables]
         else:
-            print('No error messages. Error status was: 0')
+            if (self.displayInfo == 1):
+                print('No error messages. Error status was: 0')
             return [ErrorString, Response, Variables]
 
     def quickSend(self, Command, Body, BodyType, ResponseTypes):
@@ -2046,16 +2048,16 @@ class Nanonis:
         return self.quickSend("LockInFreqSwp.PropsGet", [], [], ["H", "H", "f", "H", "f", "I", "I", "i", "*-c"])
 
     def Script_Open(self):
-    """
-    Script.Open
-    Opens the Script module.
-    Arguments: None
-    
-    Return arguments (if Send response back flag is set to True when sending request message):
-    - Error described in the Response message>Body section
-    
-    
-    """
+        """
+        Script.Open
+        Opens the Script module.
+        Arguments: None
+        
+        Return arguments (if Send response back flag is set to True when sending request message):
+        - Error described in the Response message>Body section
+        
+        
+        """
         return self.quickSend("Script.Open", [], [], [])
 
     def Script_Load(self, Script_index, Script_file_path, Load_session):
@@ -2237,16 +2239,16 @@ class Nanonis:
                               ["H", "i", "I", "+*c", "+*c"], [])
 
     def Script_LUTOpen(self):
-    """
-    Script.LUTOpen
-    Opens the LUT (Look Up Table) Editor from the Script module.
-    Arguments: None
-    
-    Return arguments (if Send response back flag is set to True when sending request message):
-    - Error described in the Response message>Body section
-    
-    
-    """
+        """
+        Script.LUTOpen
+        Opens the LUT (Look Up Table) Editor from the Script module.
+        Arguments: None
+        
+        Return arguments (if Send response back flag is set to True when sending request message):
+        - Error described in the Response message>Body section
+        
+        
+        """
         return self.quickSend("Script.LUTOpen", [], [], [])
 
     def Script_LUTLoad(self, Script_index, Script_file_path, LUT_Values):
